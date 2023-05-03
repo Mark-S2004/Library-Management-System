133b35b99ca6f3060f5fa4e8b85d58729313259c
abohoss <21p0141@eng.asu.edu.eg>
2023-05-04 00:51:32 +0300

    Setup book class

---
+++ Program/src/Book.java
@@ -0,0 +1,36 @@
+package project;
+
+public class Book {
+    private String title, author;
+    private boolean isrented;
+    public Book(){
+    }
+    public Book (String title,String author){
+        this.title=title;
+        this.author=author;
+    }
+    public Book(String title, String author, boolean isrented ) {
+        this.title = title;
+        this.author = author;
+        this.isrented = isrented;
+    }
+    public void setTitle(String s){
+        this.title=s;
+    }
+    public String getTitle(){
+        return title;
+    }
+    public void setAuthor(String a){
+        this.author=a;
+    }
+    public String getAuthor(){
+        return author;
+    }
+    public void setIsrented(boolean e){
+        this.isrented=e;
+    }
+    public boolean getIsrented(){
+        return isrented;
+}
+
+}
