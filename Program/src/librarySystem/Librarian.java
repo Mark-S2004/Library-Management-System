370b5cc6bd20f4a75a884c5927b09a732ee4e750
Mark Saleh <mark-saleh@outlook.com>
2023-05-04 02:13:11 +0300

    Develop Librarian class

---
+++ Program/src/librarySystem/Librarian.java
@@ -0,0 +1,28 @@
+package librarySystem;
+
+public class Librarian extends User{
+    public Librarian (int id, String password, String firstName, String lastName, String email, String address, int cellphone, boolean isBlocked) {
+        super(id, password, firstName, lastName, "Librarian", email, address, cellphone, isBlocked);
+    }
+    public void addBook (Book book) {
+        for (Book libraryBook: library.listBooks) {
+            if (book == libraryBook) return;
+        }
+        library.listBooks.add(book);
+    }
+    public void removeBook (Book book) {
+        library.listBooks.remove(book);
+    }
+    public void addUser (User user) {
+        for (User libraryUser: library.listUsers) {
+            if (user == libraryUser) return;
+        }
+        library.listUsers.add(user);
+    }
+    public void removeUser (User user) {
+        library.listUsers.remove(user);
+    }
+    public void blockUser (User user) {
+        user.setBlocked(true);
+    }
+}
