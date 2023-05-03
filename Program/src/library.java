908100f473ff5acaaa523f3584942953ec452d77
abohoss <21p0141@eng.asu.edu.eg>
2023-05-04 00:52:05 +0300

    Setup library class

---
+++ Program/src/library.java
@@ -0,0 +1,11 @@
+package project;
+
+import java.util.ArrayList;
+
+public class library {
+    static ArrayList<Book> listBooks=new ArrayList();
+    static ArrayList<User> listUsers=new ArrayList();
+
+    public library()(
+            )
+}
