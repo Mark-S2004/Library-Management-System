09fa50cfda7392afac7eb5a173ce7336ee04a2cf
Mark Saleh <mark-saleh@outlook.com>
2023-05-04 17:59:03 +0300

    Make listUsers and listBooks public

--- Program/src/librarySystem/library.java
+++ Program/src/librarySystem/library.java
@@ -3,6 +3,6 @@ package librarySystem;
 import java.util.ArrayList;
 
 public class library {
-    static ArrayList<Book> listBooks=new ArrayList();
-    static ArrayList<User> listUsers=new ArrayList();
+    public static ArrayList<Book> listBooks=new ArrayList();
+    public static ArrayList<User> listUsers=new ArrayList();
 }
