fb7404962dad3612c193c83c343efcd981c2bdf2
Mark Saleh <mark-saleh@outlook.com>
2023-05-04 20:02:14 +0300

    Develop Main module for testing

--- Program/src/Main.java
+++ Program/src/Main.java
@@ -19,6 +19,11 @@ public class Main {
         Book book1 = new Book("Harry", "JK", false);
         Book book2 = new Book("Percy", "to", false);
         Book book3 = new Book("test", "toto", false);
+        library.listBooks.add(book1);
+        library.listBooks.add(book2);
+        library.listBooks.add(book3);
+        library.listUsers.add(hanna);
+        library.listUsers.add(mark);
 
         start();
     }
@@ -32,9 +37,11 @@ public class Main {
             user = signup();
         }
         if (user instanceof Reader) {
-            renderReaderPage(user);
+            Reader reader=(Reader)user;
+            renderReaderPage(reader);
         } else {
-            renderLibrarianPage(user);
+            Librarian librarian=(Librarian)user;
+            renderLibrarianPage(librarian);
         }
     }
     private static User login () {
@@ -88,17 +95,130 @@ public class Main {
         library.listUsers.add(user);
         return user;
     }
-    private static void renderReaderPage(User user) {
+    private static void renderReaderPage(Reader user) {
         System.out.println("Welcome " + user.getFirstName());
         System.out.println("Choose an action:\n" +
                 "1: Search book\n" +
                 "2: Search user\n" +
                 "3: Order a book\n" +
-                "4: Rent a book");
+                "4: Rent a book\n" +
+                "0: Sign out");
         guestChoice = scanner.nextInt();
+        switch (guestChoice) {
+            case 1:
+                System.out.println("Enter book title: ");
+                String title = scanner.next();
+                user.searchBook(title);
+                break;
+            case 2:
+                System.out.println("Enter user first name: ");
+                String first = scanner.next();
+                System.out.println("Enter user last name: ");
+                String last = scanner.next();
+                User resultUser = user.searchUser(first, last);
+                resultUser.toString();
+                break;
+            case 3:
+                System.out.println("Enter book title to order: ");
+                title = scanner.next();
+                Book book = user.searchBook(title);
+                user.addToBookOrderList(book);
+                break;
+            case 4:
+                System.out.println("Enter book title to rent: ");
+                title = scanner.next();
+                book = user.searchBook(title);
+                user.rent(book);
+                break;
+            default:
+                start();
+                break;
+        }
     }
-    private static void renderLibrarianPage(User user) {
+    private static void renderLibrarianPage(Librarian user) {
         System.out.println("Welcome " + user.getFirstName());
-        System.out.println("");
+        System.out.println("Choose an action:\n" +
+                "1: Search book\n" +
+                "2: Search user\n" +
+                "3: Order a book\n" +
+                "4: Rent a book\n" +
+                "5: Block user\n" +
+                "6: Add a book to library\n" +
+                "7: Remove book from library\n" +
+                "8: Add a user to library\n" +
+                "9: Remove user from library\n" +
+                "0: Sign out");
+        guestChoice = scanner.nextInt();
+        switch (guestChoice) {
+            case 1:
+                System.out.println("Enter book title: ");
+                String title = scanner.next();
+                user.searchBook(title);
+                break;
+            case 2:
+                System.out.println("Enter user first name: ");
+                String first = scanner.next();
+                System.out.println("Enter user last name: ");
+                String last = scanner.next();
+                User resultUser = user.searchUser(first, last);
+                resultUser.toString();
+                break;
+            case 3:
+                System.out.println("Enter user first name: ");
+                first = scanner.next();
+                System.out.println("Enter user last name: ");
+                last = scanner.next();
+                User reader = user.searchUser(first, last);
+                System.out.println("Enter book title to order: ");
+                title = scanner.next();
+                Book book = user.searchBook(title);
+                user.addToBookOrderList(book, reader);
+                break;
+            case 4:
+                System.out.println("Enter book title to rent: ");
+                title = scanner.next();
+                book = user.searchBook(title);
+                user.rent(book);
+                break;
+            case 5:
+                System.out.println("Enter user first name to block: ");
+                first = scanner.next();
+                System.out.println("Enter user last name to block: ");
+                last = scanner.next();
+                reader = user.searchUser(first, last);
+                user.blockUser(reader);
+                break;
+            case 6:
+                System.out.println("Enter book title to add to library: ");
+                title = scanner.next();
+                book = user.searchBook(title);
+                user.addBook(book);
+                break;
+            case 7:
+                System.out.println("Enter book title to remove from library: ");
+                title = scanner.next();
+                book = user.searchBook(title);
+                user.removeBook(book);
+                break;
+            case 8:
+                System.out.println("Enter user first name to add to library: ");
+                first = scanner.next();
+                System.out.println("Enter user last name to add to library: ");
+                last = scanner.next();
+                reader = user.searchUser(first, last);
+                user.addUser(reader);
+                break;
+            case 9:
+                System.out.println("Enter user first name to remove from library: ");
+                first = scanner.next();
+                System.out.println("Enter user last name to remove from library: ");
+                last = scanner.next();
+                reader = user.searchUser(first, last);
+                user.removeUser(reader);
+                break;
+            default:
+                start();
+                break;
+        }
     }
 }
