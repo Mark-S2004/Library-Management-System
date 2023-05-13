9f832ddfec8d4469e42d95185e6c63d8258ed3b2
Geno212 <2100561@eng.asu.edu.eg>
2023-05-14 02:53:50 +0300

    Design Sign in Sign up Scene

--- Library-Mangement-GUI-Program/src/main/java/application/librarymangementguiprogram/HelloApplication.java
+++ Library-Mangement-GUI-Program/src/main/java/application/librarymangementguiprogram/HelloApplication.java
@@ -2,22 +2,35 @@ package application.librarymangementguiprogram;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage stage)  {
+        try {
+
+
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("signin-signup.fxml")));
+            Scene scene = new Scene(root);
+            stage.setTitle("Library");
+            stage.setScene(scene);
+            stage.show();
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        catch (Exception a) {
+            a.printStackTrace();
+        }
     }
 
-    public static void main(String[] args) {
+
+        public static void main(String[] args) {
         launch();
     }
 }
---
+++ Library-Mangement-GUI-Program/src/main/java/application/librarymangementguiprogram/SceneController.java
@@ -0,0 +1,35 @@
+package application.librarymangementguiprogram;
+
+import java.io.IOException;
+import java.util.Objects;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class SceneController {
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void switchToScene1(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("signin.fxml")));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToScene2(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("signup.fxml")));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
+
