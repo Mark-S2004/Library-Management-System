64d46be810926624bb583e00073cf6dd83bb46db
Geno212 <2100561@eng.asu.edu.eg>
2023-05-14 02:54:40 +0300

    Design Type of user in Sign up Scene

---
+++ Library-Mangement-GUI-Program/src/main/java/application/librarymangementguiprogram/SignUpController.java
@@ -0,0 +1,51 @@
+package application.librarymangementguiprogram;
+
+import application.librarymangementguiprogram.librarySystem.Librarian;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class SignUpController {
+        private Stage stage;
+        private Scene scene;
+        private Parent root;
+        boolean x;
+        @FXML
+        Button btnLibrarian = new Button();
+        @FXML
+        Button btnReader = new Button();
+
+        @FXML
+        void chooseUser(ActionEvent e) throws IOException {
+
+                if (e.getSource() == btnLibrarian) {
+                        x = false;
+                } else if (e.getSource() == btnReader) {
+                        x = true;
+                }
+
+
+                root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Signupform.fxml")));
+                stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+                scene = new Scene(root);
+                stage.setScene(scene);
+                stage.show();
+
+
+        }
+}
+
+
+
+
