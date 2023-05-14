5facdc187188c57599a1a02e84ee8c73c7589ee6
Mark Saleh <mark-saleh@outlook.com>
2023-05-14 17:33:02 +0300

    Add new User object to library when the user signs up

--- Library-Mangement-GUI-Program/src/main/java/application/librarymangementguiprogram/SignUpFormController.java
+++ Library-Mangement-GUI-Program/src/main/java/application/librarymangementguiprogram/SignUpFormController.java
@@ -1,11 +1,15 @@
 package application.librarymangementguiprogram;
 
+import application.librarymangementguiprogram.librarySystem.Librarian;
+import application.librarymangementguiprogram.librarySystem.Reader;
+import application.librarymangementguiprogram.librarySystem.library;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -16,6 +20,19 @@ public class SignUpFormController {
     private Scene scene;
     private Parent root;
     private boolean userType;
+    @FXML
+    TextField tfFirstName = new TextField();
+    @FXML
+    TextField tfLastName = new TextField();
+    @FXML
+    TextField tfEmail = new TextField();
+    @FXML
+    TextField tfPass = new TextField();
+    @FXML
+    TextField tfAddress = new TextField();
+    @FXML
+    TextField tfPhone = new TextField();
+
 
     public void setUserType(boolean x) {
         userType = x;
@@ -24,8 +41,18 @@ public class SignUpFormController {
     @FXML
     void switchToUserScene(ActionEvent event) throws IOException {
         if (userType) {
+            Reader user = new Reader(
+                    tfPass.getText(), tfFirstName.getText(), tfLastName.getText(),
+                    tfEmail.getText(), tfAddress.getText(), Double.parseDouble(tfPhone.getText())
+            );
+            library.listUsers.add(user);
             root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("reader.fxml")));
         } else {
+            Librarian user = new Librarian(
+                    tfPass.getText(), tfFirstName.getText(), tfLastName.getText(),
+                    tfEmail.getText(), tfAddress.getText(), Double.parseDouble(tfPhone.getText())
+            );
+            library.listUsers.add(user);
             root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("librarian.fxml")));
         }
         stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
