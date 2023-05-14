b5ddfd7e223e7ff81e343927a90205ae0d1c5d52
Geno212 <2100561@eng.asu.edu.eg>
2023-05-14 03:53:23 +0300

    initial sign up form  scene

--- Library-Mangement-GUI-Program/src/main/java/application/librarymangementguiprogram/SignUpController.java
+++ Library-Mangement-GUI-Program/src/main/java/application/librarymangementguiprogram/SignUpController.java
@@ -44,6 +44,16 @@ public class SignUpController {
 
 
         }
+        @FXML
+        public void switchToScene4(ActionEvent event) throws IOException {
+                root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("signin-signup.fxml")));
+                stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                scene = new Scene(root);
+                stage.setScene(scene);
+                stage.show();
+        }
+
+
 }
 
 
