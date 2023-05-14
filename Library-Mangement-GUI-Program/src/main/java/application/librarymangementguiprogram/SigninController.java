b741aa642eb07b7e9a064c5f0fec1b97d4dfe17f
Mark Saleh <mark-saleh@outlook.com>
2023-05-14 18:10:47 +0300

    Develop sign in feature

--- Library-Mangement-GUI-Program/src/main/java/application/librarymangementguiprogram/SigninController.java
+++ Library-Mangement-GUI-Program/src/main/java/application/librarymangementguiprogram/SigninController.java
@@ -1,19 +1,28 @@
 package application.librarymangementguiprogram;
+import application.librarymangementguiprogram.librarySystem.Librarian;
+import application.librarymangementguiprogram.librarySystem.User;
 import java.io.IOException;
 import java.util.Objects;
 
+import application.librarymangementguiprogram.librarySystem.library;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class SigninController {
     private Stage stage;
     private Scene scene;
     private Parent root;
+
+    @FXML
+    TextField tfEmail = new TextField();
+    @FXML
+    TextField tfPass = new TextField();
 
     public void switchToSignup(ActionEvent event) throws IOException {
         root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("signup.fxml")));
@@ -22,27 +31,32 @@ public class SigninController {
         stage.setScene(scene);
         stage.show();
     }
-    SignUpController user=new SignUpController();
     @FXML
-    void switchToUserScene(ActionEvent e) throws IOException {
-        if (user.x) {
-            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("reader.fxml")));
-            stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
-            scene = new Scene(root);
-            stage.setScene(scene);
-            stage.show();
-        } else if (!user.x) {
-            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("librarian.fxml")));
-            stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
-            scene = new Scene(root);
-            stage.setScene(scene);
-            stage.show();
+    void switchToUserScene(ActionEvent event) throws IOException {
+        System.out.println(library.listUsers);
+        User emailUser = null;
+        for (User libraryUser: library.listUsers) {
+            if (libraryUser.getEmail().equals(tfEmail.getText())) {
+                emailUser = libraryUser;
+                break;
+            }
+        }
+        if (emailUser == null) {
+            System.out.println("There is no users with this email. Sign up if don't have an account");
         } else {
-            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Signin.fxml")));
-            stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
-            scene = new Scene(root);
-            stage.setScene(scene);
-            stage.show();
+            if (emailUser.getPassword().equals(tfPass.getText())) {
+                if (emailUser instanceof Librarian) {
+                    root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("librarian.fxml")));
+                } else {
+                    root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("reader.fxml")));
+                }
+                stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                scene = new Scene(root);
+                stage.setScene(scene);
+                stage.show();
+            } else {
+                System.out.println("Wrong Password");
+            }
         }
     }
 }
