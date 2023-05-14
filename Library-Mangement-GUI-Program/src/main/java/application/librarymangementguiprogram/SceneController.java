39004013a4bdce6d00ab1b699f24e57d4ac09d45
Geno212 <2100561@eng.asu.edu.eg>
2023-05-14 03:56:00 +0300

    -signin-signup.fxml file updated -SceneController class updated

--- Library-Mangement-GUI-Program/src/main/java/application/librarymangementguiprogram/SceneController.java
+++ Library-Mangement-GUI-Program/src/main/java/application/librarymangementguiprogram/SceneController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.Objects;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
@@ -17,19 +18,41 @@ public class SceneController {
     private Parent root;
 
     public void switchToScene1(ActionEvent event) throws IOException {
-        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("signin.fxml")));
+        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("signup.fxml")));
         stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+    }
+SignUpController user=new SignUpController();
+    @FXML
+    void jumpToUser(ActionEvent e) throws IOException{
+        if (user.x==true) {
+
+                Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("reader.fxml")));
+                stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+                scene = new Scene(root);
+                stage.setScene(scene);
+                stage.show();
+            }
+        else if (user.x==false) {
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("librarian.fxml")));
+            stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+            scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+
+
+        }
+        else {
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Signin.fxml")));
+            stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+            scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        }
     }
 
-    public void switchToScene2(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("signup.fxml")));
-        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
+
     }
-}
 
