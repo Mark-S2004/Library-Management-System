4cbfb606c561657e58c5a5023d1936b77f7f5667
Mark Saleh <mark-saleh@outlook.com>
2023-05-14 15:16:13 +0300

    Rename SceneController module to SigninController for readability

--- Library-Mangement-GUI-Program/src/main/java/application/librarymangementguiprogram/SigninController.java
+++ Library-Mangement-GUI-Program/src/main/java/application/librarymangementguiprogram/SigninController.java
@@ -10,7 +10,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class SceneController {
+public class SigninController {
     private Stage stage;
     private Scene scene;
     private Parent root;
