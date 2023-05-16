2ea371cd7a43eb3924e0bb1c879b0b968fa5c27d
Mark Saleh <mark-saleh@outlook.com>
2023-05-16 14:07:33 +0300

    Develop sign out button method in readerController

--- Library-Mangement-GUI-Program/src/main/java/application/librarymangementguiprogram/readerController.java
+++ Library-Mangement-GUI-Program/src/main/java/application/librarymangementguiprogram/readerController.java
@@ -1,4 +1,26 @@
 package application.librarymangementguiprogram;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+
 public class readerController {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+    @FXML
+    void switchToSigninScene(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("signin-signup.fxml")));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
