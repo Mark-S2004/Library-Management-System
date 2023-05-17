037508b5441b17cd79846867e732311acdb76e2d
Youssef-Hany <21p0082@eng.asu.edu.eg>
2023-05-17 22:03:53 +0300

    develop readerController

--- Library-Mangement-GUI-Program/src/main/java/application/librarymangementguiprogram/readerController.java
+++ Library-Mangement-GUI-Program/src/main/java/application/librarymangementguiprogram/readerController.java
@@ -1,7 +1,9 @@
 package application.librarymangementguiprogram;
 
+import librarySystem.*;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
@@ -11,10 +13,35 @@ import javafx.stage.Stage;
 import java.io.IOException;
 import java.util.Objects;
 
+
 public class readerController {
     private Stage stage;
     private Scene scene;
     private Parent root;
+    @FXML
+    TextField tffirstname = new TextField();
+    @FXML
+    TextField tflastname = new TextField();
+    @FXML
+    TextField tfbook = new TextField();
+    Reader reader = new Reader();
+    @FXML
+    void rentBook(ActionEvent event){
+        reader.rent(reader.searchBook(tfbook.getText()));
+        System.out.println(reader.searchBook(tfbook.getText()).getIsrented());
+    }
+    @FXML
+    void searchBook(ActionEvent event){
+        reader.searchBook(tfbook.getText());
+    }
+    @FXML
+    void searchUser(ActionEvent event){
+        reader.searchUser(tffirstname.getText(),tflastname.getText());
+    }
+    @FXML
+    void orderBook(ActionEvent event){
+        reader.addToBookOrderList(reader.searchBook(tfbook.getText()));
+    }
     @FXML
     void switchToSigninScene(ActionEvent event) throws IOException {
         root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("signin-signup.fxml")));
