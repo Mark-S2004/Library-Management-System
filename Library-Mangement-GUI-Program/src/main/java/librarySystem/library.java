e02a68495f4fe53deda93bab12ef7ed54261e4e1
khattab126 <127531196+khattab126@users.noreply.github.com>
2023-05-19 04:53:13 +0300

    changes in library

--- Library-Mangement-GUI-Program/src/main/java/librarySystem/library.java
+++ Library-Mangement-GUI-Program/src/main/java/librarySystem/library.java
@@ -7,7 +7,7 @@ public class library {
 
     static {
        listBooks = new ArrayList<>();
-       listBooks.add(new Book("To Kill a Mockingbird", "Harper Lee", false));
+       listBooks.add(new Book("To Kill a Mockingbird", "Harper Lee", true));
        listBooks.add( new Book("Great Expectations", "Charles Dickens", true));
        listBooks.add(new Book("Pride and Prejudice", "Jane Austen"));
        listBooks.add(new Book("1984", "George Orwell"));
@@ -19,8 +19,8 @@ public class library {
         listUsers= new ArrayList<>();
         listUsers.add(new Reader("pass1","John","Doe","john@email.com","123 Main St",1234567890));
         listUsers.add(new Reader("pass2","Jane","Doe","jane@email.com","124 Main St",1234567891));
-        listUsers.add(new Reader("pass3","Jack","Smith","jack@email.com","125 Main St",1234567892));
-        listUsers.add( new Reader("pass4","Jill","Smith","jill@email.com","126 Main St",1234567893));
+        listUsers.add(new Librarian("pass3","Jack","Smith","jack@email.com","125 Main St",1234567892));
+        listUsers.add( new Librarian("pass4","Jill","Smith","jill@email.com","126 Main St",1234567893));
     }
 
 
